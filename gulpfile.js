56bfa4e8df244b27dd328c1f659b66d64beaa796
Sam Mason <sam.jbmason@gmail.com>
2014-06-11 12:25:48 +0100

    Tighten up and create diet

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var minifycss = require('gulp-minify-css');
 
 gulp.task('sass', function() {
-  gulp.src('scss/*.scss')
+  gulp.src('src/scss/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('src/css'))
 });
 
 gulp.task('watch', function() {
   gulp.watch('scss/*scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch'])
+gulp.task('dist',function() {
+  gulp.src('src/scss/lanes.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist'))
+    .pipe(minifycss())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['sass', 'watch']);
